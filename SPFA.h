edc759a40af341ceeeacf84aea76696c9e84ed4f
e24124260-oss <e24124260@gs.ncku.edu.tw>
2026-06-08 12:35:34 +0800

    更新SPFA.h

--- SPFA.h
+++ SPFA.h
@@ -2,16 +2,90 @@
 #define SPFA_H
 
 #include "ds.h"
-
+#include <queue>
 namespace skew {
     namespace SPFA {
         inline void spfa(DesignDB db){
-            std::vector<int> times;
-            for(auto const db.ffs : FF){
+            
+            int ffsize=db.ffs.size();
 
+            std::vector<int> times;
+            std::vector<bool> inQueue;
+            std::queue<int> q;
+            bool stop;
+
+            for (auto& FF : db.ffs) {
+                FF.targetArrivalFF=FF.clkFF;
+                FF.targetArrivalSS=FF.clkSS;
             }
+
             db.buildConstraintGraph(ConstraintKind::SETUP);
-            times.assign(db.ffs.size(),0);
+            times.assign(ffsize,0);
+            inQueue.assign(ffsize, false);
+            q =std::queue<int>();
+            stop=false;
+
+            for(int i=0;i<db.ffs.size();i++){
+                q.push(i);
+                times[i]=1;
+                inQueue[i]=true;
+            }
+
+            while(!q.empty()&&!stop){//setup SS
+                int launch = q.front();
+                q.pop();
+
+                inQueue[launch] = false;
+
+                for(auto &con : db.spfaAdj[launch]){
+                    int capture = con.to;
+
+                    if(db.ffs[launch].targetArrivalSS - db.ffs[capture].targetArrivalSS > con.weight){
+                        db.ffs[capture].targetArrivalSS = db.ffs[launch].targetArrivalSS-con.weight;
+                        if(!inQueue[capture]){
+                            q.push(capture);
+                            inQueue[capture] = true;
+                            times[capture]++;
+                            if(times[capture]>=ffsize)stop=true;
+                        }
+                    }
+                }
+            }
+
+
+            db.buildConstraintGraph(ConstraintKind::HOLD);
+            times.assign(ffsize,0);
+            inQueue.assign(ffsize, false);
+            q =std::queue<int>();
+            stop=false;
+
+            for(int i=0;i<db.ffs.size();i++){
+                q.push(i);
+                times[i]=1;
+                inQueue[i]=true;
+            }
+
+            while(!q.empty()&&!stop){//hold FF
+                int capture = q.front();
+                q.pop();
+
+                inQueue[capture] = false;
+
+                for(auto &con : db.spfaAdj[capture]){
+                    int launch = con.to;
+
+                    if(db.ffs[capture].targetArrivalFF - db.ffs[launch].targetArrivalFF > con.weight){
+                        db.ffs[launch].targetArrivalFF = db.ffs[capture].targetArrivalFF - con.weight;
+
+                        if(!inQueue[launch]){
+                            q.push(launch);
+                            inQueue[launch] = true;
+                            times[launch]++;
+                            if(times[launch]>=ffsize)stop=true;
+                        }
+                    }
+                }
+            }
 
         }
     }
